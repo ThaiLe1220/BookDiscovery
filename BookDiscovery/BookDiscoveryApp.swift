389a1e401571016e22fb70ddf238f8a76e94cf9b
ThaiLe1220 <lehongthai2000@gmail.com>
2023-09-11 15:49:57 +0700

    Initial Commit

---
+++ BookDiscovery/BookDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookDiscoveryApp.swift
+//  BookDiscovery
+//
+//  Created by Lê Ngọc Trâm on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
