7bff4e9455d6a45a1dd89fd34f0df77b2c0e749f
ThaiLe1220 <lehongthai2000@gmail.com>
2023-09-20 17:14:28 +0700

    resolve error after merging from delete_user

--- BookDiscovery/View/ContentView.swift
+++ BookDiscovery/View/ContentView.swift
@@ -18,28 +18,14 @@ struct ContentView: View {
 
     // The main body of the ContentView
     var body: some View {
-//        NavigationView {
-//            if userViewModel.isSignedIn {
-//                MainView(isOn: $isOn, userViewModel: userViewModel, bookViewModel: bookViewModel, reviewViewModel: ReviewViewModel())
-//            }
-//            else {
-//                UserSignInView(isOn: $isOn, userViewModel: userViewModel)
-//            }
-//        }.environment(\.colorScheme, isOn ? .dark : .light)
-        
-        Button {
-            
-            FireBaseDB().deleteReview(reviewID: "20230919084845_uhofmYGj9WOuDQbfNKnGTk8eOsy1_0") { result in
-                if result! {
-                    print("Success")
-                } else {
-                    print("Fail")
-                }
-
+        NavigationView {
+            if userViewModel.isSignedIn {
+                MainView( isOn: $isOn, userViewModel: userViewModel, bookViewModel: bookViewModel, reviewViewModel: ReviewViewModel())
             }
-        } label: {
-            Text("Delete Review")
-        }
+            else {
+                UserSignInView(isOn: $isOn, userViewModel: userViewModel)
+            }
+        }.environment(\.colorScheme, isOn ? .dark : .light)
     }
 }
 
